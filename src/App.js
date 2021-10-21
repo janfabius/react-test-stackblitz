d523c1019f293da8026b20fe2031c3e4a0c1f522
janfabius <adanet18@gmail.com>
2021-10-21 14:44:08 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import "./style.css";
+import stile from"./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>TESTO h1</h1>
+        <div class="ciao" style={stile}>
+          <h1>TESTO h1</h1>
+          <h2>TESTING h2</h2>
+        </div>
       <h2>TESTO h2</h2>
       <h3>TESTO h3</h3>
-      <h4>TESTO h4</h4>
-      <h5>TESTO h5</h5>
+        <div class="fabius" style={stile}>
+          <h4>TESTO h4</h4>
+          <h5>TESTO h5</h5>
+        </div>
       <h6>TESTO h5</h6>
       <h2>TESTO h2</h2>
       <p>titolo paragrafo</p>
